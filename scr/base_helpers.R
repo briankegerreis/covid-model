056e8032462b2e10c3fbb9ed82a26d2e584d3590
Brian Joseph Kegerreis <bjkegerr@silo.soic.indiana.edu>
2021-04-08 23:02:29 -0400

    implement infection helper functions

--- scr/base_helpers.R
+++ scr/base_helpers.R
@@ -59,6 +59,90 @@ probOfDeath<- function(pD,dg,qM){
   }
 }
 
+# assign attributes to population
+initialize_population_attrs = function(g, attr_list) {
+  # for things that start the same for the whole population
+  # each attr has one value
+  pop_size = len(V(g))
+  for (attr in names(attr_list) {
+    g = set_vertex_attr(g, attr, value=rep(attr_list[[attr]],pop_size))
+  }
+  return(g)
+}
+
+# assign varying attributes to population
+initialize_personal_attrs = function(g, attr_list) {
+  # for things that vary across the population
+  # each attr is the size of the population
+  for (attr in names(attr_list) {
+    g = set_vertex_attr(g, attr, value=attr_list[[attr]])
+  }
+  return(g)
+}
+
+# assign disease attributes to patient zero
+# attributes include characteristics of disease like prob of infection and recovery rate, and modifiers to patient attributes like prob of death and vaccine efficacy
+# example: modifier_p_death=1.5, modifier_vacc_efficacy=0.75 --> p_death goes from 0.01 to 0.015, vacc_efficacy goes from 0.80 to 0.60
+# attr_list = list(p_infect=p_infect, recovery_rate=recovery_rate, modifier_p_death=1, modifier_vacc_efficacy=1)
+initialize_patient_zero = function(g, patient_zero, attr_list) {
+  g = set_vertex_attr(g, "typ", patient_zero, "I") %>%
+    set_vertex_attr("t_infected", patient_zero, 0) %>%
+    set_vertex_attr("parent", patient_zero, 0) %>%
+    set_vertex_attr("lt", patient_zero, 0)
+  # give patient zero default covid attributes
+  for (attr in names(attr_list) {
+    g = set_vertex_attr(g, attr, patient_zero, attr_list[[attr]])
+  }
+  pI = attr_list[["p_infect"]]
+  rR = attr_list[["recovery_rate"]]
+  g = set_vertex_attr(g, "infection_rate", patient_zero, adjustrI(pI,rR))
+  return(g)
+}
+
+# assign disease attributes to neighbor upon infection
+infect_new_case = function(g, spreader, new_case, t, covid_attr_names) {
+  g = set_vertex_attr(g, "typ", new_case, "I") %>%
+    set_vertex_attr("parent", new_case, spreader) %>%
+    set_vertex_attr("t_infected", new_case, t) %>%
+    set_vertex_attr("lt", c(spreader,new_case), rep(t,2)) %>%
+    set_vertex_attr("n_children", spreader, vertex_attr(g, "n_children", spreader)+1)
+  # give new_case spreader's covid attributes
+  for (attr in covid_attr_names) {
+    g = set_vertex_attr(g, attr, new_case, vertex_attr(g, attr, spreader))
+    # and apply multiplicative modifiers to target attributes
+    if startsWith(attr, "modifier_") {
+      target_attr = sub("modifier_", "", attr)
+      g = set_vertex_attr(g, target_attr, new_case, vertex_attr(g, target_attr, new_case)*vertex_attr(g, attr, new_case))
+  }
+  pI = vertex_attr(g, "p_infect", new_case)
+  rR = vertex_attr(g, "recovery_rate", new_case)
+  g = set_vertex_attr(g, "infection_rate", new_case, adjustrI(pI,rR))
+  return(g)
+}
+
+# find all susceptible neighbors of a given individual
+susceptible_neighbors = function(g, x) {
+  all_neighbors = igraph::neighbors(g, x)
+  s_neighbors = all_neighbors[which(igraph::vertex_attr(g, "typ", all_neighbors)=="S")]
+  return(s_neighbors)
+}
+
+# calculate infection rate and recovery rate
+calculate_infection_recovery_rates = function(g, infected_patients) {
+  infection_rates = vertex_attr(g, "infection_rate", infected_patients)
+  s_neighbor_list = vector("list", length(infected_patients))
+  for (i in 1:length(infected_patients)) {
+    s_neighbor_list[[i]] = susceptible_neighbors(g, infected_patients[i])
+  }
+  s_neighbor_counts = lengths(s_neighbor_list)
+  # to account for vaccines that are not 100% effective, would need to sum everyone's susceptibility, or 1-vacc_efficacy
+  # using the modifier as a covid attribute should mean that this takes care of vaccine evasion as well
+  rateInfect <- sum(infection_rates*s_neighbor_counts) # expected infections today: infection rate * number of neighbors
+  recovery_rates = vertex_attr(g, "recovery_rate", infected_patients)
+  rateReco = sum(recovery_rates)
+  return(list(rateInfect=rateInfect, rateReco=rateReco, infection_rates=infection_rates, s_neighbor_counts=s_neighbor_counts))
+}
+
 # can't tell what these functions do but I think they have something to do with summarizing results after the simulations
 act <- function(i,dfsir,df,ncl){
   if(df[i,1] == 'R'){
---
+++ scr/base_sim_slim.R
@@ -0,0 +1,131 @@
+# M: number of sims
+# mc: number of cores
+# N: population
+# d: expected degree
+# lamb: network degree tuning parameter
+# pD: [p1, p2], where p1 is death probability for highly connected individuals, p2 is for less connected indivs
+# rR: recovery rate
+# pI: prob of infection
+# t: max time
+# q: fraction of vulnerable indivs
+# nIs: minimum number of infections for a successful sim
+# pre: output folder prefix
+# ij: I think this keeps track of which sim is running
+
+#covid <- function(M,mc,N,d,lamb,pD,rR,pI,t,q,nIs,pre,ij){
+covid = function(num_sim, num_cores,
+                 pop_size, d_connect, lamb,
+                 p_death, recovery_rate, p_infect,
+                 t_max, f_vulnerable, min_cases, pre, ij) {
+  hiaux <- c()
+  while (T) {
+    # set up the graph
+    if(lamb > 0) {
+      typ <- rep(0,pop_size)
+      g <- block_power_matrix(matrix(d_connect/pop_size),lamb,typ)
+    }
+    else {g <- igraph::erdos.renyi.game(pop_size,d_connect/pop_size)}
+    ns <- setdiff(1:pop_size,igraph::V(g))
+    if(length(ns)>0) {
+      g <- igraph::add_vertices(g,length(ns))
+    }
+    nb <- igraph::adjacent_vertices(g,1:N)
+    neighbors_list = adjacent_vertices(g, 1:pop_size)
+    dg <- igraph::degree(g)
+    vulnerable_quantile <- quantile(dg,c(f_vulnerable))
+    # these could be set in a yaml config file
+    pop_attrs = list(typ="S", t_infected=NA, t_resolved=NA, parent=NA, n_children=0, lt=NA, p_infect=0, recovery_rate=0, infection_rate=0)
+    personal_attrs = list(degree=dg, p_death=ifelse(dg<vulnerable_quantile, p_death[2], p_death[1]))
+    g = initialize_population_attributes(g, pop_attrs) %>%
+      initialize_person_attributes(g, personal_attrs)
+    k <- 1
+    patient_zero = sample(which(dg>0),1) # randomly select patient zero
+    # these could be set in a yaml config file
+    covid_attrs = list(p_infect=p_infect, recovery_rate=recovery_rate, modifier_p_death=1)
+    g = initialize_patient_zero(g, patient_zero, covid_attrs)
+    str <- paste0(' ',patient_zero,' ;')
+    t <- 0
+    total_infections = 1
+    while(T) {
+      #### g = vaccinate(g, vacc_rate, strategy, test_for_eligibility) # vaccinate 1% per day, either most connected or random, OK vaccinating R patients or S only?
+      infected_patients = which(vertex_attr(g, "typ")=="I")
+      num_infected = length(infected_patients)
+      rates = calculate_infection_recovery_rates(g, infected_patients)
+      rateInfect = rates$infect
+      rateReco = rates$reco
+      ratemin <- rateInfect + rateReco
+      if(ratemin == 0) {
+        break # the outbreak ended
+      }
+      else {
+        texp <- rexp(1,rate = ratemin) # I think this is a modified time step based on the number of people
+        if(t > t_max) {
+          break
+        }
+        t <- t+texp
+        if(runif(1) < rateInfect/ratemin) { # an infection occurs
+          total_infections = total_infections + 1
+          spreader = sample(infected_patients, 1, prob=rates$infection_rates*rates$s_neighbor_counts/rateInfect) # find the spreader, weighted by infection rate and susceptible neighbors
+          new_case = sample(susceptible_neighbors(g, spreader), 1) # find the new case among the spreader's susceptible neighbors
+          #### g = infect_and_mutate(g, spreader, new_case, mutation_prob, mutation_rate)
+          g = infect_new_case(g, spreader, new_case, t, names(covid_attr))
+          # str <- newick(df$f[ni],ni,t-df$lt[df$f[ni]],str) # I think this makes a tree to keep track of infections
+        }
+        else { # a recovery or death occurs
+          patient = sample(infected_patients, 1)
+          outcome = if(rbinom(1,1, vertex_attr(g, "p_death", patient)==1){"D"}else{"R"}
+          g = set_vertex_attr(g, "typ", patient, outcome)
+          g = set_vertex_attr(g, "t_resolved", patient, t)
+          # str <- newick(li[ix],F,t-df$lt[li[ix]],str)
+        }
+      }
+      k <- k+1
+    }
+    hiaux <- c(hiaux,length(which(vertex_attr(g, "typ")!="S")) # keep track of how many people are not susceptible
+    if(nI  > nIs || t>tStop) { # arrive here by having zero new events at this time point or by running out of time. # restart sim if not enough infections happened
+      break
+    }
+  }
+  # write.table(str, file = paste0(pre,N,'/','NW','/',ij,'.txt'),
+  #             sep = "\t",row.names = FALSE,col.names = FALSE,append=F)
+  # write.table(data.frame(hi = hiaux,lvl = pre, N= N), 
+  #             file = paste0(pre,N,'/','HI','/',ij,'.txt'),
+  #             sep = "\t",row.names = FALSE,col.names = FALSE,append=F)
+  # write.table(df, 
+  #             file = paste0(pre,N,'/','DF','/',ij,'.txt'),
+  #             sep = "\t",row.names = FALSE,col.names = FALSE,append=F)
+  # write.table(which(df$typ == 'S'), 
+  #             file = paste0(pre,N,'/','G','/S',ij,'.txt'),
+  #             sep = "\t",row.names = FALSE,col.names = FALSE,append=F)
+  # write.table(data.frame(dg = dg,lvl = pre, N= N ),file = paste0(pre,N,'/','DG','/',ij,'.txt'),
+  #             sep ='\t', row.names = F,col.names = F,append = F)
+  # write.table(igraph::as_edgelist(g),file = paste0(pre,N,'/','G','/',ij,'.txt'),sep='\t',
+  #             row.names = F,col.names = F,append = T)
+  # write.table( dfSIR(dfpreSIR(df),N),file = paste0(pre,N,'/','SIR','/',ij,'.txt'),
+  #             sep = "\t",row.names = FALSE,col.names = FALSE,append=F)
+  # write.table(igraph::as_edgelist(g),
+  #             file = paste0(pre,N,'/G','/',ij,'.txt'),sep='\t',
+  #             row.names = F,col.names = F,append = T)
+}
+
+#COVID_control <- function(M,mc,N,d,lamb,pD,rR,pI,t,q,nIs,pre){
+COVID_control = function(num_sim, num_cores,
+                         pop_size, d_connect, lamb,
+                         p_death, recovery_rate, p_infect,
+                         t_max, f_vulnerable, min_cases, pre){
+  dir.create(paste0(pre,pop_size))
+  dir.create(paste0(pre,pop_size,'/','HI'))
+  dir.create(paste0(pre,pop_size,'/','DG'))
+  dir.create(paste0(pre,pop_size,'/','G'))
+  dir.create(paste0(pre,pop_size,'/','NW'))
+  dir.create(paste0(pre,pop_size,'/','DF'))
+  dir.create(paste0(pre,pop_size,'/','SIR'))
+  if(mc == 0){
+    #invisible(lapply(1:M, function(i) covid(M,mc,N,d,lamb,pD,rR,pI,t,q,nIs,pre,i)))
+    invisible(lapply(1:num_sim, function(i) covid(num_sim,num_cores,pop_size,d_connect,lamb,p_death,recovery_rate,p_infect,t_max,f_vulnerable,min_cases,pre,i)))
+  }
+  else(
+    #invisible(parallel::mclapply(1:M,function(i) covid(M,mc,N,d,lamb,pD,rR,pI,t,q,nIs,pre,i),mc.cores = mc))
+    invisible(parallel::mclapply(1:num_sim,function(i) covid(num_sim,num_cores,pop_size,d_connect,lamb,p_death,recovery_rate,p_infect,t_max,f_vulnerable,min_cases,pre,i),mc.cores = num_cores))
+  )
+}
